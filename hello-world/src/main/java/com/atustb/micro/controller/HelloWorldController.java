65310504835d9154abec6a58f3a3f30922419c9d
wenjunlong8 <wenjunlong88_easy@163.com>
2020-06-25 03:00:50 +0800

    @ConfigurationProperties的使用

--- hello-world/src/main/java/com/atustb/micro/controller/HelloWorldController.java
+++ hello-world/src/main/java/com/atustb/micro/controller/HelloWorldController.java
@@ -1,16 +1,29 @@
 package com.atustb.micro.controller;
 
+import com.atustb.micro.config.Coach;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping(value = "/micro")
+@Slf4j
 public class HelloWorldController {
+
+    @Resource
+    private Coach coach;
 
     @GetMapping
     public String home(){
             return "hello,this is the home of the hello-world";
+    }
+
+    @RequestMapping("/coach")
+    public Coach getCoach(){
+        return coach;
     }
 
 }
